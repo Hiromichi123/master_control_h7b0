fe18bb8b540c0eef9486cdd47d7a7006a396d8bb
Hiromichi123 <154042836+Hiromichi123@users.noreply.github.com>
2024-06-07 19:24:52 +0800

    revise

--- User/emm42/emm42.c
+++ User/emm42/emm42.c
@@ -1,11 +1,5 @@
-//
-// Created by XuJiang on 2023/5/17.
-//
 #include "main.h"
 #include "emm42.h"
-#ifdef EMM42_DEBUG
-#include "stdio.h"
-#endif
 
 /**
   * @brief 步进电机串口初始化
@@ -23,7 +17,7 @@ int emmMotorUartInit(emm42_motor *motor, UART_HandleTypeDef *uart, UChar address
 /**
   * @brief 步进电机串口数据发送
   * @note None
-  * @param 电机信息结构体，发送的指令结构体
+  * @param 电机信息结构体，发送的指令结构体（头详情见文件）
   * @retval 成功标志
   */
 int emmMotorSend(emm42_motor motor, command sendCommand){
@@ -44,28 +38,11 @@ int emmMotorSend(emm42_motor motor, command sendCommand){
     p++;
 
     int commandLength = p - hexData;
-#ifdef EMM42_DEBUG
-    printf("uart=%d\t", *motor.uart);
-    printChar(hexData, commandLength);
-#endif
 
-#ifdef STM32
     uint8_t uselessData;
     while (HAL_UART_Receive(motor.uart, &uselessData, 1, 1) != HAL_TIMEOUT);
     HAL_Delay(20);
     HAL_UART_Transmit(motor.uart, (uint8_t *) hexData, commandLength, HAL_MAX_DELAY);
-#endif
-
-#ifdef TM4
-    //清空缓冲区
-    while (UARTCharGetNonBlocking(*motor.uart) != -1);
-    SysDelay_ms(10);
-
-    for (int i = 0; i < commandLength; i++){
-        UARTCharPut(*motor.uart, hexData[i]);
-    }
-
-#endif
 
     return SUCCESS;
 }
--- User/emm42/emm42.h
+++ User/emm42/emm42.h
@@ -1,55 +1,90 @@
-//#define EMM42_DEBUG
-//#define TM4
-#define STM32
-
-
 #ifndef EMM42_H
 #define EMM42_H
 
 #include "stdbool.h"
 #include "stdint.h"
+#include "usart.h"
 
 typedef unsigned char UChar;
 #define FORWARD 1
 #define BACK 0
 
-
-#ifdef EMM42_DEBUG
-#define UART_HandleTypeDef int
-UChar HexToAscii(UChar c);
-int printChar(UChar hexData[], int commandLength);
-#endif
-
-#ifdef TM4
-#define SUCCESS 1
-#define ERROR 0
-typedef uint32_t UART_HandleTypeDef;
-#include "../main.h"
-#endif
-
-#ifdef STM32
-#include "usart.h"
-#endif
-
+// 和emm42通信的结构体
 typedef struct {
     UART_HandleTypeDef *uart;
     UChar address;
     UChar checkByte;
 } emm42_motor;
 
+// 和emm42通信的命令结构体
 typedef struct {
     UChar commandByte;
     UChar params[10];
     unsigned int paramsLength;
 } command;
 
+// 初始化EMM电机的UART
+// 参数:
+// - motor: 指向emm42_motor结构体的指针
+// - uart: 指向UART_HandleTypeDef结构体的指针
+// - address: 电机的地址
+// - checkByte: 通信的校验字节
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emmMotorUartInit(emm42_motor *motor, UART_HandleTypeDef *uart, UChar address, UChar checkByte);
+
+// 向EMM电机发送命令
+// 参数:
+// - motor: emm42_motor结构体
+// - sendCommand: 要发送的命令
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emmMotorSend(emm42_motor motor, command sendCommand);
 
+// 从EMM电机读取编码器数据
+// 参数:
+// - motor: emm42_motor结构体
+// - dataOfEncoder: 存储编码器数据的指针
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emm42ReadEncoder(emm42_motor motor, uint16_t* dataOfEncoder);
+
+// 从EMM电机读取位置数据
+// 参数:
+// - motor: emm42_motor结构体
+// - dataOfPosition: 存储位置数据的指针
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emm42ReadPosition(emm42_motor motor, int32_t* dataOfPosition);
 
+// 启用或禁用EMM电机
+// 参数:
+// - motor: emm42_motor结构体
+// - enable: true启用，false禁用
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emm42Enable(emm42_motor motor, bool enable);
+
+// 设置EMM电机的速度
+// 参数:
+// - motor: emm42_motor结构体
+// - direction: 旋转方向（FORWARD或BACK）
+// - speed: 速度值
+// - acceleration: 加速度值
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emm42SetSpeed(emm42_motor motor, bool direction, uint16_t speed, uint8_t acceleration);
+
+// 设置EMM电机的位置
+// 参数:
+// - motor: emm42_motor结构体
+// - direction: 运动方向（FORWARD或BACK）
+// - speed: 速度值
+// - acceleration: 加速度值
+// - pulse: 脉冲值
+// - wait: true等待完成，false立即继续
+// 返回值:
+// - 如果成功返回0，如果发生错误返回-1
 int emm42SetPosition(emm42_motor motor, bool direction, uint16_t speed, uint8_t acceleration, uint32_t pulse, bool wait);
+
 #endif //EMM42_H
